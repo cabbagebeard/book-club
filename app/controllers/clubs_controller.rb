ae6035436a84c401430921105419c2a7984f0342
Kevin Windisch <kwindisch1992@gmail.com>
2017-05-15 22:43:42 +0000

    Top navbar

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,17 @@
 class ClubsController < ApplicationController
     def index
     end
+    
+    def new
+        @club = Club.new
+    end
+    
+    def create
+        @club = Club.new(club_params)
+        if @club.save
+            redirect_to @club
+        else
+            render 'new'
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
- root "home#index"
+    resources :clubs
+    root "home#index"
 end
