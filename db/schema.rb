e48b465d6303dc849eb45e2885f72eaff9d968a3
Kevin Windisch <kwindisch1992@gmail.com>
2017-06-21 20:39:59 +0000

    migration for user avatars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612185542) do
+ActiveRecord::Schema.define(version: 20170621203826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 20170612185542) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
