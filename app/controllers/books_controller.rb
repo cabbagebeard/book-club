1b0bb460679ee60671033189bf5b0bdef64efd56
Kevin Windisch <kwindisch1992@gmail.com>
2017-06-30 21:57:47 +0000

    begin setting up searching for books before having users create new ones

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @books = Book.all
+        @book = Book.all
     end
     
     def new
