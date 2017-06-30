9c215a3354f953aa08a9fbb1a58ac32aecb6d774
Kevin Windisch <kwindisch1992@gmail.com>
2017-06-30 22:07:17 +0000

    add error pages for not found

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
     resources :clubs, :books
     root "home#index"
+    
+    get '*unmatched_route', to: 'error#not_found'
 end
