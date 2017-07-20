ec52f94fbdac17dd9ef456aefb567bb4bb834639
Kevin Windisch <kwindisch1992@gmail.com>
2017-07-20 18:40:25 +0000

    add discussions to books

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
     resources :clubs do
-      resources :books
+      resources :books do
+        resources :discussions
+      end
     end
     
     root "home#index"
