83e7d68183ffa94f646db2a559f80d347716aa5a
Kevin Windisch <kwindisch1992@gmail.com>
2017-07-25 20:18:33 +0000

    add members resources to clubs routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
     resources :clubs do
+      resources :members, :controller => "memberships"
       resources :books do
         resources :discussions do
           resources :comments
