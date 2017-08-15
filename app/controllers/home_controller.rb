698f5d7802568b5b3a38185bfb62112ea123c8d3
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-15 20:56:28 +0000

    better formatting and information on clubs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
     def index
         user_clubs = current_user.clubs if current_user.clubs.count >= 1
         @active_club = user_clubs.order('updated_at DESC').first
+        @current_reading = @active_club.readings.where(:current => true)[0]
+        @current_book = @current_reading.book
     end
 end
