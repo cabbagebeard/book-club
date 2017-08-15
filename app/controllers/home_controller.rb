5a355385f3e89bfbe6e7c3c3094c72cefcd2644c
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-15 21:11:34 +0000

    make sure there is a user before assigning variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
     def index
-        user_clubs = current_user.clubs if current_user.clubs.count >= 1
-        @active_club = user_clubs.order('updated_at DESC').first
-        @current_reading = @active_club.readings.where(:current => true)[0]
-        @current_book = @current_reading.book
+        if current_user
+            user_clubs = current_user.clubs if current_user.clubs.count >= 1
+            @active_club = user_clubs.order('updated_at DESC').first
+            @current_reading = @active_club.readings.where(:current => true)[0]
+            @current_book = @current_reading.book
+        end
     end
 end
