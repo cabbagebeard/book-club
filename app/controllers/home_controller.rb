223a8427cc89c5485531d6d92aba75f12375ed48
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-18 18:45:14 +0000

    accomodate for having no clubs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-        if current_user
-            @user_clubs = current_user.clubs if current_user.clubs.count >= 1
+        if current_user and current_user.clubs.count >= 1
+            @user_clubs = current_user.clubs
             @active_club = @user_clubs.order('updated_at DESC').first
             @current_reading = @active_club.readings.where(:current => true)[0]
             @current_book = @current_reading.book
