951be0ab85fb1a76b46e632d8ee6338418d6ff2f
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-28 16:50:14 +0000

    modify migrations to clubs

--- db/migrate/20170826180540_add_announcements_to_clubs.rb
+++
@@ -1,5 +0,0 @@
-class AddAnnouncementsToClubs < ActiveRecord::Migration
-  def change
-    add_column :clubs, :announcement, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826180540) do
+ActiveRecord::Schema.define(version: 20170828164855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,8 @@ ActiveRecord::Schema.define(version: 20170826180540) do
   create_table "clubs", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.text     "announcement"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "comments", force: :cascade do |t|
