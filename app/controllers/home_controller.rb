d64d224c23a4556d7a5543c89fd3024ff4f6f169
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-28 17:13:35 +0000

    error handling for clubs with no books

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
         if current_user and current_user.clubs.count >= 1
             @user_clubs = current_user.clubs
             @active_club = @user_clubs.order('updated_at DESC').first
-            @current_reading = @active_club.readings.where(:current => true)[0]
-            @current_book = @current_reading.book
+            if @active_club.readings.count >= 1
+                @current_reading = @active_club.readings.where(:current => true)[0]
+                @current_book = @current_reading.book
+            end
         end
     end
 end
