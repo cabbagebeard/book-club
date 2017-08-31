3705204a681ddba5dc624b50e50bd19b9a89ef90
Kevin Windisch <kwindisch1992@gmail.com>
2017-08-31 19:02:33 +0000

    create relationship between announcements and clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,5 +6,5 @@ class Club < ActiveRecord::Base
     has_many :memberships
     has_many :users, through: :memberships
 
-    validates :name, presence: true, length: { minimum: 5 }
+    validates :name, presence: true, length: { minimum: 3 }
 end
