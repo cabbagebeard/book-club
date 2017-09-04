bdfa99d1e1896c74258d409e7d8cf29cfd1bdd50
Kevin Windisch <kwindisch1992@gmail.com>
2017-09-04 17:25:53 +0000

    build out new and create methods

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,3 +1,28 @@
 class AnnouncementsController < ApplicationController
+    def new
+        @club = Club.find(params[:club_id])
+        
+        @announcement = @club.announcements.build(announcement_params)
+    end
+    
+    def create
+        @club = Club.find(params[:club_id])
+        
+        @announcement = @club.announcements.build(announcement_params)
+        @club.announcements << @announcement
+        @announcement.user_id << current_user.id
+        
+        if @discussion.save
+            redirect_to(@club)
+            flash[:success] = "Announcement created!"
+        else
+            redirect_to(@club)
+            flash[:danger] = "Something went wrong while posting your announcement. Please try again."
+        end
+    end
 
+private
+    def announcement_params
+        params.require(:announcement).permit(:subject, :body, :club_id, :user_id)
+    end
 end
