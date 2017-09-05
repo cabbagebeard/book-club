47f8d1f2ae205c0d42d9758303f6d1e0cda132fc
Kevin Windisch <kwindisch1992@gmail.com>
2017-09-05 18:16:03 +0000

    input validations

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,4 +1,7 @@
 class Announcement < ActiveRecord::Base
     belongs_to :club
     belongs_to :user
+    
+    validates :subject, presence: true, length: { minimum: 3 }
+    validates :body, presence: true, length: { minimum: 3 }
 end
